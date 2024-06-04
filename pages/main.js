5525c94418ca10d60f2d2767eba48428dd386f03
p2r3 <p2r3@p2r3.com>
2024-06-04 16:37:03 +0000

    Fix time not displaying after submitting a demo

--- pages/main.js
+++ pages/main.js
@@ -433,13 +433,14 @@ var homepageInit = async function () {
     
     const safeNote = encodeURIComponent(note);
 
+    let data;
     try {
 
       const response = await fetch(`/api/leaderboard/submit/${category}/${safeNote}`, {
         method: "POST",
         body: formData,
       });
-      const data = await response.json();
+      data = await response.json();
 
       if (typeof data === "string") switch (data) {
         case "ERR_LOGIN":
@@ -473,7 +474,7 @@ var homepageInit = async function () {
     leaderboard = await (await fetch("/api/leaderboard/get")).json();
     displayLeaderboard(leaderboardCategorySelect.value);
 
-    return showPopup("Success", "Your run has been submitted!<br>Time: " + ticksToString(data));
+    return showPopup("Success", "Your run has been submitted!<br>Time: " + ticksToString(data.time));
 
   }
 
