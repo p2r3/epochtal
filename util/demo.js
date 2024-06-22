5e66b36153fac1a04e0d5f916814e08b50f88ab7
p2r3 <p2r3@p2r3.com>
2024-06-22 14:32:32 +0000

    Fix file checksum mismatch events in demos

--- util/demo.js
+++ util/demo.js
@@ -105,8 +105,8 @@ module.exports = async function (args, context = epochtal) {
         if (event.type === "file") {
           const path = event.value.path;
           if (
-            path.startsWith("./portal2_tempcontent/") ||
-            path.startsWith("./update/")
+            path.includes("/common/Portal 2/portal2_tempcontent/") ||
+            path.includes("/common/Portal 2/update/")
           ) {
             return `Significant file \`${event.value.path}\` has mismatched checksum \`${event.value.sum}\`.`;
           }
