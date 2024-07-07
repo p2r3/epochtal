86fb2ef547133f269b00a92ed77e0fb0cc3db9df
PancakeTAS <pancake@mgnet.work>
2024-07-08 01:05:25 +0200

    docs(api/archive) document archive endpoint handler

--- api/archive.js
+++ api/archive.js
@@ -1,5 +1,17 @@
 const archive = require("../util/archive.js");
 
+/**
+ * Handles `/api/archive/` endpoint requests. This endpoint supports the following commands:
+ *
+ * - `list`: Lists all sorted archives
+ * - `leaderboard`: Fetches the leaderboard of a given archive. Throws ERR_NAME if the archive does not exist.
+ * - `config`: Fetches the configuration of a given archive. Throws ERR_NAME if the archive does not exist.
+ * - `demo`: Fetches the proof of a given player in a given category. Throws ERR_ARGS if the arguments are invalid and ERR_NOTFOUND if no proof is found.
+ *
+ * @param args The arguments for the api request
+ * @param request The http request object
+ * @returns {string|Response<BunFile>} The response of the api request
+ */
 module.exports = async function (args, request) {
 
   const [command, name] = args;
@@ -7,17 +19,20 @@ module.exports = async function (args, request) {
   switch (command) {
 
     case "list": {
-    
+
+      // Fetch a sorted list of all archives
       return archive(["list"]);
-    
+
     }
 
     case "leaderboard":
     case "config": {
 
+      // Try to fetch the leaderboard or configuration of a given archive
       const context = await archive(["get", name]);
       if (!context) return "ERR_NAME";
 
+      // Return the requested data
       if (command === "leaderboard") return context.data.leaderboard;
       return context.data.week;
 
