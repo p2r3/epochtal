7cf4c8d3815b2053cbb6037af336aa21a9a6c3c0
p2r3 <p2r3@p2r3.com>
2024-07-10 19:55:21 +0000

    Support sending attachments

--- util/discord.js
+++ util/discord.js
@@ -5,18 +5,19 @@ const keys = require("../../keys.js");
 module.exports = async function (args, context = epochtal) {
 
   const command = args[0];
-  const message = args.slice(1).join(" ");
+  const content = Array.isArray(args[2]) ? args[1] : args.slice(1).join(" ");
+  const files = Array.isArray(args[2]) ? args[2] : [];
 
   switch (command) {
 
     case "announce":
 
-      discordClient.channels.cache.get(context.data.discord.announce).send(message);
+      await discordClient.channels.cache.get(context.data.discord.announce).send({ content, files });
       return "SUCCESS";
 
     case "report":
       
-      discordClient.channels.cache.get(context.data.discord.report).send(message)
+      await discordClient.channels.cache.get(context.data.discord.report).send({ content, files });
       return "SUCCESS";
   
   }
