e4761c573d45e8f43048421de15fe2a86bb96038
PancakeTAS <pancake@mgnet.work>
2024-07-11 21:45:53 +0200

    docs(portal2/main): document spplice main js

--- defaults/portal2/main.js
+++ defaults/portal2/main.js
@@ -1,36 +1,52 @@
+/*
+ * Due to valve's decision to break most coop commands on orange, a few workarounds are needed to make coop possible again.
+ * This main.js file is loaded by spplice and feeds off the console output to relay commands to the game.
+ */
+
 let input = "";
 let pongIgnore = 0;
 
+// Handle console output
 onConsoleOutput(function (data) {
 
+  // Append new data to the input buffer
   input += data.toString().replaceAll("\r", "");
 
+  // Split the input buffer by newline characters
   const lines = input.split("\n");
   input = lines.pop();
 
+  // Iterate over each completed new line
   for (let i = 0; i < lines.length; i ++) {
 
+    // Relay commands to the game
     if (lines[i].startsWith("[SendToConsole] ")) {
       SendToConsole(lines[i].slice(16));
       continue;
     }
 
+    // Respond to coop portalgun pings
     if (lines[i].includes("[coop_portal_ping]")) {
-      
+
+      // Hide the message from the onscreen chat and respond with a pong
       SendToConsole("hud_saytext_time 0;say [coop_portal_pong]");
+      // Up pongIgnore to ignore the next two pings, as they are echos of the same ping on the wrong client
       pongIgnore = 2;
-      
+
       continue;
     }
 
+    // Respond to coop portalgun pongs
     if (lines[i].includes("[coop_portal_pong]")) {
-      
+
+      // Only respond to the correct pongs
       if (pongIgnore > 0) {
         pongIgnore --;
         continue;
       }
       pongIgnore = 1;
 
+      // Trigger coop vscript to update the portals
       SendToConsole("script ::coopUpdatePortals()");
 
       continue;
