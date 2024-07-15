3f8b7854f652e44cf8bdc3380199b94a61eca174
PancakeTAS <pancake@mgnet.work>
2024-07-15 16:45:35 +0200

    docs(demo): document demo utility

--- util/demo.js
+++ util/demo.js
@@ -6,7 +6,13 @@ const tmppath = require("./tmppath.js");
 const discord = require("./discord.js");
 const testcvar = require("./testcvar.js");
 
-// Allows specifying just the filename of the demo as a shorthand for the full path
+/**
+ * Extends the demo file path if it is not already an absolute path.
+ * Allows specifying just the filename of the demo as a shorthand for the full path
+ *
+ * @param {string} file The file path to extend
+ * @returns {string} The extended file path
+ */
 function extendFilePath (file) {
   if (!fs.existsSync(file)) {
     return `${__dirname}/../demos/${file}`;
@@ -14,6 +20,12 @@ function extendFilePath (file) {
   return file;
 }
 
+/**
+ * Decompresses an XZ-compressed file
+ *
+ * @param {string} file File to decompress
+ * @returns {string} The decompressed file path
+ */
 async function decompressXZ (file) {
 
   await $`xz -dkf ${file}`.quiet();
@@ -21,11 +33,19 @@ async function decompressXZ (file) {
 
 }
 
+/**
+ * Parses a demo file with UntitledParser and returns the steamid and partner steamid
+ *
+ * @param {string} file Path to the file to parse
+ * @returns {object} Object containing the steamid and partner steamid
+ */
 async function parseDump (file) {
 
+  // Make sure file is decompressed
   const originalFile = file;
   if (file.endsWith(".dem.xz")) file = await decompressXZ(file);
 
+  // Parse the demo into tmp path and read the dump
   const outputPath = await tmppath();
   fs.mkdirSync(outputPath);
 
@@ -40,14 +60,17 @@ async function parseDump (file) {
 
   // i dont really wanna do this yet
 
+  // Parse the steamid from the dump
   const steamidSplit = dump.split("GUID: STEAM_1:");
 
   const parts = steamidSplit[1].split("\n")[0].split(":");
   output.steamid = (76561197960265728n + BigInt(parts[0]) + BigInt(parts[1]) * 2n).toString();
 
+  // Parse the partners steam id from the dump
   if (steamidSplit.length > 2) {
     let partner = output.steamid;
     let i = 2;
+    // Skip steamids until we find one that is not the same as the main steamid
     while (partner === output.steamid && i < steamidSplit.length) {
       const currParts = steamidSplit[i++].split("\n")[0].split(":");
       partner = (76561197960265728n + BigInt(currParts[0]) + BigInt(currParts[1]) * 2n).toString();
@@ -61,11 +84,19 @@ async function parseDump (file) {
 
 }
 
+/**
+ * Parses a demo file with mluggs demo parser
+ *
+ * @param {string} file Path to the file to parse
+ * @returns {object} Parsed demo data
+ */
 async function parseMDP (file) {
 
+  // Make sure file is decompressed
   const originalFile = file;
   if (file.endsWith(".dem.xz")) file = await decompressXZ(file);
 
+  // Parse the demo into json
   const text = (await $`cd ${`${__dirname}/../bin/mdp-json`} && ./mdp ${file}`.text()).replaceAll("\\", "\\\\");
   const output = JSON.parse(text);
   if (originalFile !== file) fs.unlinkSync(file);
@@ -74,8 +105,27 @@ async function parseMDP (file) {
 
 }
 
+/**
+ * Demo verification verdicts.
+ * VERDICT_SAFE: No signs of potential cheating
+ * VERDICT_ILLEGAL: Clear illegal utilities used
+ * VERDICT_UNSURE: No certain verdict
+ */
 const [VERDICT_SAFE, VERDICT_UNSURE, VERDICT_ILLEGAL] = [0, 1, 2];
 
+/**
+ * Handles the `demo` utility call. This utility is used to parse and verify demo files.
+ *
+ * The following subcommands are available:
+ * - `dump`: Dump steamid and partner steamid from the demo file
+ * - `mdp`: Parse a demo file with mluggs demo parser
+ * - `parse`: Fully parse a demo file and extract time and portal count
+ * - `verify`: Verify a demo file for cheating
+ *
+ * @param {string[]} args The arguments for the call
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {object|string} The output of the call
+ */
 module.exports = async function (args, context = epochtal) {
 
   const [command, file] = args;
@@ -84,36 +134,44 @@ module.exports = async function (args, context = epochtal) {
 
     case "dump": {
 
+      // Extend demo file path if it is not already an absolute path
       const path = extendFilePath(file);
       if (!fs.existsSync(path)) throw new UtilError("ERR_FILE", args, context);
 
+      // Parse the demo file
       return await parseDump(path);
 
     }
 
     case "mdp": {
 
+      // Extend demo file path if it is not already an absolute path
       const path = extendFilePath(file);
       if (!fs.existsSync(path)) throw new UtilError("ERR_FILE", args, context);
 
+      // Parse the demo file using mdp
       return await parseMDP(path);
 
     }
 
     case "parse": {
 
+      // Extend demo file path if it is not already an absolute path
       const path = extendFilePath(file);
       if (!fs.existsSync(path)) throw new UtilError("ERR_FILE", args, context);
 
+      // Fully parse the demo file
       const mdp = await parseMDP(path);
       const dump = await parseDump(path);
 
+      // Extract the time and portal count from the demo
       const output = {
         ...dump,
         time: null,
         portals: 0
       };
 
+      // Filter through demo events to find the time and portal count
       for (const event of mdp.demos[0].events) {
 
         if (event.type === "speedrun") {
@@ -127,6 +185,7 @@ module.exports = async function (args, context = epochtal) {
 
       }
 
+      // Fail if the time or steamid is missing
       if (!output.time || !output.steamid) throw new UtilError("ERR_PARSE", args, context);
 
       return output;
@@ -135,11 +194,14 @@ module.exports = async function (args, context = epochtal) {
 
     case "verify": {
 
+      // Extend demo file path if it is not already an absolute path
       const path = extendFilePath(file);
       if (!fs.existsSync(path)) throw new UtilError("ERR_FILE", args, context);
 
+      // Parse the demo file using mdp
       const mdp = await parseMDP(path);
 
+      // Ensure tickrate is correct
       if (Math.abs(mdp.demos[0].tps - 60.00) > 0.01) {
         return `Tickrate mismatch. Expected 60.00, got ${mdp.tps.toFixed(2)}.`;
       }
@@ -149,6 +211,7 @@ module.exports = async function (args, context = epochtal) {
 
       for (const event of mdp.demos[0].events) {
 
+        // Ensure demo is submitted within 1 hour
         if (event.type === "timestamp") {
           if (Date.now() - Date.parse(event.value) > 1000 * 60 * 60) {
             return `Demo was recorded more than 1h ago.`;
@@ -156,6 +219,7 @@ module.exports = async function (args, context = epochtal) {
           continue;
         }
 
+        // Ensure all significant files passed the validation check
         if (event.type === "file") {
           const path = event.value.path.toLowerCase();
           if (
@@ -167,6 +231,7 @@ module.exports = async function (args, context = epochtal) {
           continue;
         }
 
+        // Ensure no sar or demo mismatches are present
         if (event.type === "sarsum") {
           return `SAR checksum mismatch, got \`${event.value}\`.`;
         }
@@ -174,6 +239,7 @@ module.exports = async function (args, context = epochtal) {
           return `Demo checksum mismatch.`;
         }
 
+        // Ensure the demo is on the correct map
         if (event.type === "cvar") {
           if (event.val.cvar === "host_map") {
             const expected = context.data.week.map.file + ".bsp";
@@ -183,6 +249,7 @@ module.exports = async function (args, context = epochtal) {
           }
         }
 
+        // Ensure the demo is not using illegal commands
         if (event.type === "cvar" || event.type === "cmd") {
 
           const cvar = (event.type === "cvar" ? event.val.cvar : event.value.split(" ")[0]).trim().toLowerCase();
