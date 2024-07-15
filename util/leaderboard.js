c107aa60544167100a138e8ce131c58f37b4fd92
p2r3 <p2r3@p2r3.com>
2024-07-15 21:06:49 +0000

    Remove trailing whitespace

--- util/leaderboard.js
+++ util/leaderboard.js
@@ -6,7 +6,7 @@ const weeklog = require("./weeklog.js");
 module.exports = async function (args, context = epochtal) {
 
   const [command, category, steamid] = args;
-  
+
   const data = context.data.leaderboard;
   const file = context.file.leaderboard;
 
@@ -20,9 +20,9 @@ module.exports = async function (args, context = epochtal) {
     if (!(category in data)) data[category] = [];
     lb = data[category];
   }
-  
+
   switch (command) {
-    
+
     case "list": {
 
       return Object.keys(data);
@@ -30,48 +30,48 @@ module.exports = async function (args, context = epochtal) {
     }
 
     case "get": {
-    
+
       let output = [];
 
       if (lb === undefined) throw new UtilError("ERR_CATEGORY", args, context);
 
       let placement = 1;
       for (let i = 0; i < lb.length; i ++) {
-        
+
         if (i !== 0 && lb[i].time !== lb[i-1].time) {
           placement ++;
         }
         lb[i].placement = placement;
-        
+
         output.push(lb[i]);
-        
+
       }
-      
+
       return output;
-    
+
     }
-      
+
     case "remove": {
-      
+
       if (lb === undefined) throw new UtilError("ERR_CATEGORY", args, context);
-      
+
       const idx = lb.findIndex(function (curr) {
         return curr.steamid === steamid;
       });
-      
+
       if (idx === -1) throw new UtilError("ERR_NOTFOUND", args, context);
-      
+
       data[category].splice(idx, 1);
       if (file) Bun.write(file, JSON.stringify(data));
-      
+
       await weeklog(["add", steamid, category, 0, 0], context);
 
       return "SUCCESS";
-      
+
     }
 
     case "add": {
-    
+
       if (lb === undefined) throw new UtilError("ERR_CATEGORY", args, context);
       if (categoryData.lock) throw new UtilError("ERR_LOCKED", args, context);
 
@@ -84,11 +84,11 @@ module.exports = async function (args, context = epochtal) {
       if (isNaN(time) || time <= 0) throw new UtilError("ERR_TIME", args, context);
 
       if (note.length > 200) throw new UtilError("ERR_NOTE", args, context);
-      
+
       if (!("portals" in categoryData)) throw new UtilError("ERR_CATEGORY", args, context);
       const countPortals = categoryData.portals;
       if (countPortals && isNaN(portals)) throw new UtilError("ERR_PORTALS", args, context);
-      
+
       const partners = context.data.week.partners;
 
       if (categoryData.points && !categoryData.coop && steamid in partners) {
@@ -103,9 +103,9 @@ module.exports = async function (args, context = epochtal) {
         }
       });
       if (oldRunIndex !== -1) lb.splice(oldRunIndex, 1);
-      
+
       const newRun = { steamid, time, note };
-      
+
       let inserted = false;
 
       if (countPortals) {
@@ -127,7 +127,7 @@ module.exports = async function (args, context = epochtal) {
           lb.splice(i, 0, newRun);
           inserted = true;
           break;
-        
+
         }
 
       } else {
@@ -139,9 +139,9 @@ module.exports = async function (args, context = epochtal) {
           lb.splice(i, 0, newRun);
           inserted = true;
           break;
-        
+
         }
-        
+
       }
 
       if (!inserted) lb.push(newRun);
@@ -153,15 +153,15 @@ module.exports = async function (args, context = epochtal) {
       return "SUCCESS";
 
     }
-    
+
     case "edit": {
-  
+
       if (lb === undefined) throw new UtilError("ERR_CATEGORY", args, context);
       if (categoryData.lock) throw new UtilError("ERR_LOCKED", args, context);
-  
+
       const note = args[3];
       if (note === undefined) throw new UtilError("ERR_ARGS", args, context);
-  
+
       if (note.length > 200) throw new UtilError("ERR_NOTE", args, context);
 
       const run = lb.find(curr => curr.steamid === steamid);
@@ -169,9 +169,9 @@ module.exports = async function (args, context = epochtal) {
 
       if (file) Bun.write(file, JSON.stringify(data));
       return "SUCCESS";
-  
+
     }
-  
+
     throw new UtilError("ERR_COMMAND", args, context);
 
   }
