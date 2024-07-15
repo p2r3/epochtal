acb61b4d6ed9f3070c42a1d9fd7aa85d1bdc09bd
PancakeTAS <pancake@mgnet.work>
2024-07-15 16:45:50 +0200

    docs(discord): document discord utility

--- util/discord.js
+++ util/discord.js
@@ -2,12 +2,27 @@ const UtilError = require("./error.js");
 
 const keys = require("../../keys.js");
 
+/**
+ * Handles the `discord` utility call. This utility is used to interact with the discord server.
+ *
+ * The following subcommands are available:
+ * - `announce`: Send an announcement to the announcements channel
+ * - `report`: Send a report to the reports channel
+ * - `update`: Send an update to the updates channel
+ *
+ * @param {string[]} args The arguments for the call
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {string} The output of the call
+ */
 module.exports = async function (args, context = epochtal) {
 
   const command = args[0];
+
+  // Parse the content and files from the arguments
   const content = Array.isArray(args[2]) ? args[1] : args.slice(1).join(" ");
   const files = Array.isArray(args[2]) ? args[2] : [];
 
+  // Send appropriate message to the discord server
   switch (command) {
 
     case "announce":
