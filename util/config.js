df5d1899469c0c0fb0ae8ec20f88f2d9e48f583a
Soni <soni@recalstudios.net>
2024-07-16 12:09:10 +0200

    docs(config): document fixdata subcommand in config.js

--- util/config.js
+++ util/config.js
@@ -6,7 +6,7 @@ const UtilError = require("./error.js");
  * The following subcommands are available:
  * - `get`: Get the in `args[1]` specified configuration value.
  * - `edit`: Edit the in `args[1]` specified configuration value to `args[2]`.
- * - `fixdate`: ???
+ * - `fixdate`: Set the `data.week.date` field of the given context to be the first day of the current week at 12:00 UTC.
  *
  * @param {string[]} args The arguments for the call
  * @param {unknown} context The context on which to execute the call (defaults to epochtal)
@@ -48,14 +48,16 @@ module.exports = async function (args, context = epochtal) {
 
     case "fixdate": {
 
-      // TODO: what does this do?
-
       const today = new Date();
 
+      // Get the first day of the current week at 12:00 UTC
       let mapReleaseDate = new Date(today.getTime() - (today.getDay() + 6) % 7 * 24 * 60 * 60 * 1000);
       mapReleaseDate.setUTCHours(12, 0, 0, 0);
+
+      // Convert date object to a UNIX timestamp by removing milliseconds
       mapReleaseDate = Math.floor(mapReleaseDate.getTime() / 1000);
 
+      // Write the correct date to file
       week.date = mapReleaseDate;
       if (file) Bun.write(file, JSON.stringify(week));
 
