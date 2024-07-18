4b412018a125819df601bd6e4220a32942da26de
p2r3 <p2r3@p2r3.com>
2024-07-18 23:42:53 +0000

    Add Spplice index flushing

--- util/flush.js
+++ util/flush.js
@@ -13,6 +13,7 @@ module.exports = async function (args, context = epochtal) {
       context.data.leaderboard = await context.file.leaderboard.json();
       context.data.users = await context.file.users.json();
       context.data.week = await context.file.week.json();
+      context.data.spplice.index = await context.file.spplice.index.json();
 
       return "SUCCESS";
 
@@ -23,6 +24,7 @@ module.exports = async function (args, context = epochtal) {
       await Bun.write(context.file.leaderboard, JSON.stringify(context.data.leaderboard));
       await Bun.write(context.file.users, JSON.stringify(context.data.users));
       await Bun.write(context.file.week, JSON.stringify(context.data.week));
+      await Bun.write(context.file.spplice.index, JSON.stringify(context.data.spplice.index));
 
       return "SUCCESS";
 
