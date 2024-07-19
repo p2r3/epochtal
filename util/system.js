fa9843883f003cb9f1ec58f93cee80340571b98a
PancakeTAS <pancake@mgnet.work>
2024-07-19 20:30:59 +0200

    docs(system): document system utility

--- util/system.js
+++ util/system.js
@@ -3,6 +3,17 @@ const UtilError = require("./error.js");
 const process = require("process");
 const { spawn } = require("child_process");
 
+/**
+ * Handles the `system` utility call. This utility is used to restart or shut down the system.
+ *
+ * The following subcommands are available:
+ * - `restart`: Restart the system after delay seconds `args[1]` (default 3)
+ * - `shutdown`: Shut down the system after a delay seconds `args[1]` (default 3)
+ *
+ * @param {string[]} args The arguments for the call
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {string} The output of the call
+ */
 module.exports = function (args, context) {
 
   const [command] = args;
@@ -14,13 +25,14 @@ module.exports = function (args, context) {
       let time = args[1];
       if (time === undefined) time = 3;
 
+      // Detach the process and restart the system
       setTimeout(function () {
 
         spawn("bun", ["run", "main.js"], {
           detached: true,
           stdio: "ignore"
         });
-  
+
         process.exit();
 
       }, 1000 * time);
@@ -34,6 +46,7 @@ module.exports = function (args, context) {
       let time = args[1];
       if (time === undefined) time = 3;
 
+      // Detach the process and shut down the system
       setTimeout(function () {
         process.exit();
       }, 1000 * time);
