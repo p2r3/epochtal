d3df3a1c79999802ffc81f178dd96444f69add0a
Soni <soni@recalstudios.net>
2024-07-19 22:16:25 +0200

    docs(profiledata): document the profiledata utility
    Wow, the code here is actually so clean I don't even need inline code
    comments! I much approve :+1:

--- util/profiledata.js
+++ util/profiledata.js
@@ -1,6 +1,22 @@
 const UtilError = require("./error.js");
 const UtilPrint = require("./print.js");
 
+/**
+ * Handles the `profiledata` utility call. This is used to get or modify a user's profile data.
+ *
+ * The following subcommands are available:
+ * - `get`: Get the profile of the user with the provided SteamID. Throws ERR_STEAMID if the user does not exist.
+ * - `flush`: Write profile data of the specified user to disk. Throws ERR_STEAMID if the user does not exist.
+ * - `edit`: Write the specified key (`args[2]`) and value (`args[3]`) to the user with the provided SteamID. Then flush
+ * the changes.
+ * - `forceadd`/`add`: Create a profile with the provided SteamID. Uses `args[2]` as the user avatar. Throws ERR_EXISTS
+ * if the profile already exists, unless using the `forceadd` subcommand.
+ * - `remove`: Delete the profile at the provided SteamID. Throws ERR_STEAMID if the user does not exist.
+ *
+ * @param args The arguments for the call. `args[1]` should contain the appropriate SteamID.
+ * @param context The context on which to execute the call (defaults to epochtal)
+ * @returns {Promise<*|string>} The output of the call
+ */
 module.exports = async function (args, context = epochtal) {
 
   const [command, steamid, key, value] = args;
@@ -11,7 +27,7 @@ module.exports = async function (args, context = epochtal) {
   switch (command) {
 
     case "get": {
-      
+
       if (!(steamid in profiles)) throw new UtilError("ERR_STEAMID", args, context);
       return profiles[steamid];
 
@@ -24,7 +40,7 @@ module.exports = async function (args, context = epochtal) {
 
       const profile = profiles[steamid];
       if (command === "edit") profile[key] = value;
-      
+
       if (profilesPath) {
         const dataPath = `${profilesPath}/${steamid}/data.json`;
         await Bun.write(dataPath, JSON.stringify(profile));
@@ -60,7 +76,7 @@ module.exports = async function (args, context = epochtal) {
       return "SUCCESS";
 
     }
-    
+
     case "remove": {
 
       if (!(steamid in profiles)) throw new UtilError("ERR_STEAMID", args, context);
@@ -73,9 +89,9 @@ module.exports = async function (args, context = epochtal) {
       }
 
       return "SUCCESS";
-      
+
     }
-  
+
   }
 
   throw new UtilError("ERR_COMMAND", args, context);
