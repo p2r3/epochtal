d50c063e0c663dfdf65885ec4a27629c667382a5
PancakeTAS <pancake@mgnet.work>
2024-07-19 20:42:37 +0200

    docs(print): document print utility

--- util/print.js
+++ util/print.js
@@ -1,6 +1,15 @@
 const { appendFileSync } = require("node:fs");
 const events = require("./events.js");
 
+/**
+ * Handles the `utilPrint` utility call. This utility is used to print a message to the console.
+ *
+ * The message is also appended to a file in the util directory and logged as an event.
+ *
+ * @param {string[]} message The message to print
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {void} The output of the call
+ */
 module.exports = function UtilPrint (message, context = epochtal) {
 
   if (Array.isArray(message)) message = message.join(" ");
