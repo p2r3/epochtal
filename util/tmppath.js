fb433215c0c838f95f10f05e35d936b70277ff6e
PancakeTAS <pancake@mgnet.work>
2024-07-19 20:40:40 +0200

    docs(tmppath): document tmppath utility

--- util/tmppath.js
+++ util/tmppath.js
@@ -1,6 +1,12 @@
 const { existsSync } = require("node:fs");
 const path = require("path");
 
+/**
+ * Generates a random alphanumeric string of a given length
+ *
+ * @param {Number} len The length of the string to generate
+ * @returns {String} The generated string
+ */
 function randomString (len) {
 
   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -14,6 +20,13 @@ function randomString (len) {
 
 };
 
+/**
+ * Handles the `tmppath` utility call. This utility is used to generate a temporary file path.
+ *
+ * @param {Number} len The length of the random string to generate (defaults to 8)
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {string} The output of the call
+ */
 module.exports = async function (len = 8, context = null) {
 
   if (Array.isArray(len)) len = len[0];
