3565857e5300af559782fa851cdcf138dca5e81e
PancakeTAS <pancake@mgnet.work>
2024-07-22 23:28:40 +0200

    docs(lobbies): document lobbies utility

--- util/lobbies.js
+++ util/lobbies.js
@@ -4,6 +4,22 @@ const users = require("./users.js");
 const events = require("./events.js");
 const { createHash } = require("crypto");
 
+/**
+ * Handles the `lobbies` utility call. This utility is used to manage game lobbies.
+ *
+ * The following subcommands are available:
+ * - `list`: List all lobbies
+ * - `get`: Get a lobby
+ * - `getdata`: Get lobby data
+ * - `create`: Create a new lobby
+ * - `join`: Join a lobby
+ * - `rename`: Rename a lobby
+ * - `password`: Set a lobby password
+ *
+ * @param {string[]} args The arguments for the call
+ * @param {unknown} context The context on which to execute the call (defaults to epochtal)
+ * @returns {string[]|string} The output of the call
+ */
 module.exports = async function (args, context = epochtal) {
 
   const [command, name, password, steamid] = args;
@@ -15,12 +31,14 @@ module.exports = async function (args, context = epochtal) {
 
     case "list": {
 
+      // Return a list of all lobbies
       return lobbies.list;
 
     }
 
     case "get": {
 
+      // Return a lobby if it exists
       if (!(name in lobbies.list)) throw new UtilError("ERR_NAME", args, context);
       return lobbies.list[name];
 
@@ -28,6 +46,7 @@ module.exports = async function (args, context = epochtal) {
 
     case "getdata": {
 
+      // Return lobby data if it exists
       if (!(name in lobbies.data)) throw new UtilError("ERR_NAME", args, context);
       return lobbies.data[name];
 
@@ -35,10 +54,12 @@ module.exports = async function (args, context = epochtal) {
 
     case "create": {
 
+      // Ensure the name is valid
       const cleanName = name.trim();
       if (!cleanName || cleanName.length > 50) throw new UtilError("ERR_NAME", args, context);
       if (cleanName in lobbies.list || cleanName in lobbies.data) throw new UtilError("ERR_EXISTS", args, context);
 
+      // Create a new lobby and data
       const hashedPassword = createHash("sha256").update(password).digest("base64");
 
       const listEntry = {
@@ -53,14 +74,18 @@ module.exports = async function (args, context = epochtal) {
       lobbies.list[cleanName] = listEntry;
       lobbies.data[cleanName] = dataEntry;
 
+      // Write the lobbies to file if it exists
       if (file) Bun.write(file, JSON.stringify(lobbies));
 
+      // Create the event handlers for the lobby
       const auth = steamid => listEntry.players.includes(steamid);
       const disconnect = async function (steamid) {
 
+        // Remove the player from the lobby
         const index = listEntry.players.indexOf(steamid);
         if (index !== -1) listEntry.players.splice(index, 1);
 
+        // Find the lobby name
         let lobbyName;
         for (const name in lobbies.list) {
           if (lobbies.list[name] === listEntry) {
@@ -69,9 +94,11 @@ module.exports = async function (args, context = epochtal) {
           }
         }
 
+        // Brodcast the leave to clients
         const eventName = "lobby_" + lobbyName;
         await events(["send", eventName, { type: "lobby_leave", steamid }], context);
 
+        // Delete the lobby if it is empty
         if (listEntry.players.length === 0) {
           delete lobbies.list[lobbyName];
           delete lobbies.data[lobbyName];
@@ -83,6 +110,7 @@ module.exports = async function (args, context = epochtal) {
 
       };
 
+      // Create the lobby creation event
       await events(["create", "lobby_" + cleanName, auth, null, null, disconnect], context);
 
       return "SUCCESS";
@@ -91,6 +119,7 @@ module.exports = async function (args, context = epochtal) {
 
     case "join": {
 
+      // Ensure the user and lobby exist
       const user = await users(["get", steamid], context);
       if (!user) throw new UtilError("ERR_STEAMID", args, context);
 
@@ -98,9 +127,13 @@ module.exports = async function (args, context = epochtal) {
       if (lobbies.data[name].password && lobbies.data[name].password !== password) throw new UtilError("ERR_PASSWORD", args, context);
       if (lobbies.list[name].players.includes(steamid)) throw new UtilError("ERR_EXISTS", args, context);
 
+      // Add the player to the lobby
       lobbies.list[name].players.push(steamid);
+
+      // Write the lobbies to file if it exists
       if (file) Bun.write(file, JSON.stringify(lobbies));
 
+      // Brodcast the join to clients
       await events(["send", "lobby_" + name, { type: "lobby_join", steamid }], context);
 
       return "SUCCESS";
@@ -111,10 +144,12 @@ module.exports = async function (args, context = epochtal) {
 
       const newName = args[2].trim();
 
+      // Ensure the new name is valid
       if (newName in lobbies.list || newName in lobbies.data) throw new UtilError("ERR_EXISTS", args, context);
       if (!(name in lobbies.list && name in lobbies.data)) throw new UtilError("ERR_NAME", args, context);
       if (!newName || newName.length > 50) throw new UtilError("ERR_NEWNAME", args, context);
 
+      // Rename the lobby
       const listEntry = lobbies.list[name];
       const dataEntry = lobbies.data[name];
 
@@ -129,19 +164,25 @@ module.exports = async function (args, context = epochtal) {
       await events(["send", eventName, { type: "lobby_name", newName }], context);
       await events(["rename", eventName, "lobby_" + newName], context);
 
+      // Write the lobbies to file if it exists
       if (file) Bun.write(file, JSON.stringify(lobbies));
+
       return "SUCCESS";
 
     }
 
     case "password": {
 
+      // Ensure the lobby exists
       if (!(name in lobbies.list && name in lobbies.data)) throw new UtilError("ERR_NAME", args, context);
 
+      // Set the lobby password
       const hashedPassword = createHash("sha256").update(password).digest("base64");
       lobbies.data[name].password = password ? hashedPassword : false;
 
+      // Write the lobbies to file if it exists
       if (file) Bun.write(file, JSON.stringify(lobbies));
+
       return "SUCCESS";
 
     }
