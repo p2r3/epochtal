6ceb1b0d4410926e2cd8618f24202fb83e2e4438
Misheus <60829538+Misheus@users.noreply.github.com>
2025-09-13 00:54:27 +0300

    fix(code style): change indentation size in new files to 2 as it should've been from the beginning

--- pages/shared.js
+++ pages/shared.js
@@ -4,21 +4,21 @@
  * @param {number} t The number of ticks
  * @returns {string} The formatted string
  */
-function ticksToString (t) {
+function ticksToString(t) {
 
-    // Split the ticks into hours, minutes, and seconds
-    let output = "";
-    const hrs = Math.floor(t / 216000),
-        min = Math.floor(t / 3600),
-        sec = t % 3600 / 60;
+  // Split the ticks into hours, minutes, and seconds
+  let output = "";
+  const hrs = Math.floor(t / 216000),
+    min = Math.floor(t / 3600),
+    sec = t % 3600 / 60;
 
-    // Format the output string
-    if (hrs !== 0) output += `${hrs}:${min % 60 < 10 ? "0" : ""}${min % 60}:`;
-    else if (min !== 0) output += `${min}:`;
-    if (sec < 10) output += "0";
-    output += sec.toFixed(3);
+  // Format the output string
+  if (hrs !== 0) output += `${hrs}:${min % 60 < 10 ? "0" : ""}${min % 60}:`;
+  else if (min !== 0) output += `${min}:`;
+  if (sec < 10) output += "0";
+  output += sec.toFixed(3);
 
-    return output;
+  return output;
 
 }
 
@@ -29,42 +29,42 @@ function ticksToString (t) {
  * @param {string[]} categoryList List of categories
  * @returns {object[]} Array of objects representing profilelog entries
  */
-function decodeProfileLog (buffer, categoryList) {
+function decodeProfileLog(buffer, categoryList) {
 
-    const log = [];
+  const log = [];
 
-    // each entry is 10 bytes long
-    for (let i = 0; i < buffer.length; i += 10) {
+  // each entry is 10 bytes long
+  for (let i = 0; i < buffer.length; i += 10) {
 
-        const entry = {};
+    const entry = {};
 
-        // 1 byte - category index
-        entry.category = categoryList[buffer[i]];
+    // 1 byte - category index
+    entry.category = categoryList[buffer[i]];
 
-        // 4 bytes - run time in ticks
-        entry.time = 0;
-        for (let j = 0; j < 4; j ++) {
-            entry.time += buffer[i + 1 + j] * Math.pow(256, 3 - j);
-        }
-
-        // 1 byte - portal count
-        entry.portals = buffer[i + 5];
-
-        // 4 bytes - seconds since start of week 0
-        entry.timestamp = 0;
-        for (let j = 0; j < 4; j ++) {
-            entry.timestamp += buffer[i + 6 + j] * Math.pow(256, 3 - j);
-        }
-
-        log.push(entry);
-
+    // 4 bytes - run time in ticks
+    entry.time = 0;
+    for (let j = 0; j < 4; j++) {
+      entry.time += buffer[i + 1 + j] * Math.pow(256, 3 - j);
     }
 
-    return log;
+    // 1 byte - portal count
+    entry.portals = buffer[i + 5];
+
+    // 4 bytes - seconds since start of week 0
+    entry.timestamp = 0;
+    for (let j = 0; j < 4; j++) {
+      entry.timestamp += buffer[i + 6 + j] * Math.pow(256, 3 - j);
+    }
+
+    log.push(entry);
+
+  }
+
+  return log;
 
 }
 
 
-if(typeof global === "object") {
-    module.exports = { ticksToString, decodeProfileLog };
+if (typeof global === "object") {
+  module.exports = {ticksToString, decodeProfileLog};
 }
--- pages/stream/common.js
+++ pages/stream/common.js
@@ -4,32 +4,32 @@
  * @returns {Promise<{leaderboard: Object, config: Object, users: Object, controllerSocket: WebSocket}>}
  */
 async function initialSetup() {
-    const leaderboard = await (await fetch("/api/leaderboard/get")).json();
-    const config = await (await fetch("/api/config/get")).json();
-    const users = await (await fetch("/api/users/get")).json();
+  const leaderboard = await (await fetch("/api/leaderboard/get")).json();
+  const config = await (await fetch("/api/config/get")).json();
+  const users = await (await fetch("/api/users/get")).json();
 
-    // Create the stream controller event topic if it doesn't exist
-    await fetch("/util/events/create/streamController", { method: "POST" });
-    // Connect to the event WebSocket
-    const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    const controllerSocket = new WebSocket(`${protocol}://${window.location.host}/api/events/connect`);
-    controllerSocket.onopen = async function (event) {
-        const token = await (await fetch("/api/events/auth/streamController")).json();
-        controllerSocket.send(token);
-    };
-    // Every 30 seconds, send an empty object as a sort of heartbeat ping
-    setInterval(() => controllerSocket.send("{}"), 30000);
+  // Create the stream controller event topic if it doesn't exist
+  await fetch("/util/events/create/streamController", {method: "POST"});
+  // Connect to the event WebSocket
+  const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+  const controllerSocket = new WebSocket(`${protocol}://${window.location.host}/api/events/connect`);
+  controllerSocket.onopen = async function (event) {
+    const token = await (await fetch("/api/events/auth/streamController")).json();
+    controllerSocket.send(token);
+  };
+  // Every 30 seconds, send an empty object as a sort of heartbeat ping
+  setInterval(() => controllerSocket.send("{}"), 30000);
 
-    /**
-     * Sends WebSocket events to the stream controller event topic, which the UI, game, and controller listen to
-     * @param {unknown} data Data to send, converted to a JSON string
-     */
-    window.sendToController = async function (data) {
+  /**
+   * Sends WebSocket events to the stream controller event topic, which the UI, game, and controller listen to
+   * @param {unknown} data Data to send, converted to a JSON string
+   */
+  window.sendToController = async function (data) {
 
-        const dataString = encodeURIComponent(JSON.stringify(data));
-        await fetch(`/util/events/send/streamController/${dataString}`, { method: "POST" });
+    const dataString = encodeURIComponent(JSON.stringify(data));
+    await fetch(`/util/events/send/streamController/${dataString}`, {method: "POST"});
 
-    };
+  };
 
-    return {leaderboard, config, users, controllerSocket};
+  return {leaderboard, config, users, controllerSocket};
 }
